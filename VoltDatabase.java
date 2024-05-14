694a4c25d5655e956eee2d53fbe9a9162b268dcb
Abdullah Abdin <>
2024-05-14 21:49:09 +0300

    added get next id functions

--- VoltDatabase.java
+++ VoltDatabase.java
@@ -11,40 +11,49 @@ public class VoltDatabase {
         client.createConnection(servers);
     }
 
-    private int lastPackageId = 0;
-    private int lastCustomerId = 0;
-    private int lastBalanceId = 0;
+    // Method to get next package id
+    public int getNextPackageId() throws Exception {
+        VoltTable response = client.callProcedure("@AdHoc","SELECT PACKAGE_ID FROM PACKAGE ORDER BY PACKAGE_ID DESC LIMIT 1;").getResults()[0];
+        if(response.advanceRow())
+        return (int) response.getLong("PACKAGE_ID") + 1;
+        
+        throw new Exception("error while getting next package id");
+    }
+    
+    // Method to get next customer id
+    public int getNextCustomerId() throws Exception {
+        VoltTable response = client.callProcedure("@AdHoc","SELECT CUST_ID FROM CUSTOMER ORDER BY CUST_ID DESC LIMIT 1;").getResults()[0];
+        if(response.advanceRow())
+            return (int) response.getLong("CUST_ID") + 1;
+            
+            throw new Exception("error while getting next customer id");
+        }
+        
+    // Method to get next balance id
+    public int getNextBalanceId() throws Exception {
 
-    private int generateNextPackageId() {
-        lastPackageId++; // Increment the last assigned package ID
-        return lastPackageId;
-    }
-    
-    private int generateNextCustomerId() {
-        lastCustomerId++; // Increment the last assigned balance ID
-        return lastBalanceId;
-    }
-    
-    private int generateNextBalanceId() {
-        lastBalanceId++; // Increment the last assigned balance ID
-        return lastBalanceId;
+        VoltTable response = client.callProcedure("@AdHoc","SELECT BALANCE_ID FROM BALANCE ORDER BY BALANCE_ID DESC LIMIT 1;").getResults()[0];
+        if(response.advanceRow())
+            return (int) response.getLong("BALANCE_ID") + 1;
+
+        throw new Exception("error while getting next balance id");
     }
 
     // Method to insert data into PACKAGE table
     public void insertPackage(String packageName, double price, int minutes, int data, int sms, int period) throws Exception {
-        int packageId = generateNextPackageId();
+        int packageId = getNextPackageId();
         client.callProcedure("@AdHoc", "INSERT INTO PACKAGE VALUES (?, ?, ?, ?, ?, ?);", packageId, packageName, price, minutes, data, sms, period);
     }
 
     // Method to insert data into CUSTOMER table
     public void insertCustomer(String msisdn, String name, String surname, String email, String password, String status, String securityKey) throws Exception {
-        int custId = generateNextCustomerId();
+        int custId = getNextCustomerId();
         client.callProcedure("@AdHoc", "INSERT INTO CUSTOMER VALUES (?, ?, ?, ?, ?, CURRENT_TIMESTAMP, ?, ?);", custId, msisdn, name, surname, email, password, status, securityKey);
     }
 
     // Method to insert data into BALANCE table
     public void insertBalance(int packageId, int custId, int partitionId, int minutes, int sms, int data, double price, double moneyBalance) throws Exception {
-        int balanceId = generateNextBalanceId();
+        int balanceId = getNextBalanceId();
         client.callProcedure("@AdHoc", "INSERT INTO BALANCE VALUES (?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP, NULL, ?, ?);", balanceId, packageId, custId, partitionId, minutes, sms, data, price, moneyBalance);
     }
 
